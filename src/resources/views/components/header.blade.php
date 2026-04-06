3a6dc6db6b9aaec1be9e52061fb3c537685ea018
Tsutomu Shinzawa <tsutomu.shinzawa320@gmail.com>
2026-04-06 15:55:14 +0900

    header search 画面調整

--- src/resources/views/components/header.blade.php
+++ src/resources/views/components/header.blade.php
@@ -17,34 +17,52 @@
         </a>
     </div>
     @if( !in_array(Route::currentRouteName(), ['register', 'login', 'verification.notice']) )
-    <form class="header_search" action="/" method="get">
-        @csrf
-        <select name="area" class="header_search--select">
-            <option value="">All area</option>
-            <option value="北海道">北海道</option>
-            <option value="東北">東北</option>
-            <option value="関東">関東</option>
-            <option value="中部">中部</option>
-            <option value="近畿">近畿</option>
-            <option value="中国">中国</option>
-            <option value="四国">四国</option>
-            <option value="九州・沖縄">九州・沖縄</option>
-        </select>
-        <select name="genre" class="header_search--select">
-            <option value="">ジャンル</option>
-            <option value="寿司">寿司</option>
-            <option value="居酒屋">居酒屋</option>
-            <option value="焼肉">焼肉</option>
-            <option value="ラーメン">ラーメン</option>
-            <option value="イタリアン">イタリアン</option>
-            <option value="中華">中華</option>
-        </select>
+    <div class="header-search__wrapper">
+        <form class="header-search" action="/" method="get">
+            @csrf
+            <div class="select-wrapper">
+                <select name="area" class="header-search__select">
+                    <option value="">All area</option>
+                    <option value="北海道">北海道</option>
+                    <option value="東北">東北</option>
+                    <option value="関東">関東</option>
+                    <option value="中部">中部</option>
+                    <option value="近畿">近畿</option>
+                    <option value="中国">中国</option>
+                    <option value="四国">四国</option>
+                    <option value="九州・沖縄">九州・沖縄</option>
+                </select>
 
-        <input id="inputElement" class="header_search--input" type="text" name="search" placeholder="なにをお探しですか？">
-        <button id="buttonElement" class="header_search--button">
-            <img src="{{ asset('img/search_icon.jpeg') }}" alt="検索アイコン" style="height:100%;">
-        </button>
-    </form>
+            </div>
+            <div class="line-wrapper">
+                <div class="line">
+                </div>
+            </div>
+            <div class="select-wrapper">
+                <select name="genre" class="header-search__select">
+                    <option value="">All genre</option>
+                    <option value="寿司">寿司</option>
+                    <option value="居酒屋">居酒屋</option>
+                    <option value="焼肉">焼肉</option>
+                    <option value="ラーメン">ラーメン</option>
+                    <option value="イタリアン">イタリアン</option>
+                    <option value="中華">中華</option>
+                </select>
+
+            </div>
+            <div class="line-wrapper">
+                <div class="line">
+                </div>
+            </div>
+            <div class="search-box">
+                <i class="fas fa-search"></i> <!-- Font Awesomeの検索アイコン -->
+                <input id="inputElement" class="header-search--input" type="text" name="search" placeholder="Search...">
+            </div>
+            <button id="buttonElement" class="header-search--button">
+                <img src="{{ asset('img/search_icon.jpeg') }}" alt="検索アイコン" style="height:100%;">
+            </button>
+        </form>
+    </div>
     @endif
 
 
--- src/resources/views/shop/index.blade.php
+++ src/resources/views/shop/index.blade.php
@@ -4,6 +4,8 @@
 
 @section('css')
 <link rel="stylesheet" href="{{ asset('css/index.css') }}">
+<!-- Font Awesome CDN (アイコン用) -->
+<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.15.4/css/all.min.css">
 @endsection
 
 @section('content')
