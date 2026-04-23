6a9bbd335b68c4dcf1be9bee32fae15fa1e93671
Tsutomu Shinzawa <tsutomu.shinzawa320@gmail.com>
2026-04-23 20:33:41 +0900

    ReservationTest 2nd

--- src/tests/Feature/ReservationTest.php
+++ src/tests/Feature/ReservationTest.php
@@ -38,5 +38,36 @@ class ReservationTest extends TestCase
         $errors = session('errors');
         $this->assertEquals('年月を入力してください', $errors->first('date'));
     }
-}
 
+    public function test_reservation_validation_time()
+    {
+        $this->actingAs(User::factory()->create(['email_verified_at' => now()]));
+
+        $response = $this->post('/reservation', [
+            'shop_id' => 1,
+            'date' => "2026-05-01",
+            'time' => "",
+            'number_of_people' => 2,
+        ]);
+        $response->assertStatus(302);
+        $response->assertSessionHasErrors('time');
+        $errors = session('errors');
+        $this->assertEquals('来店時刻を入力してください', $errors->first('time'));
+    }
+
+    public function test_reservation_validation_number_of_people()
+    {
+        $this->actingAs(User::factory()->create(['email_verified_at' => now()]));
+
+        $response = $this->post('/reservation', [
+            'shop_id' => 1,
+            'date' => "2026-05-01",
+            'time' => "19:00",
+            'number_of_people' => 2,
+        ]);
+        $response->assertStatus(302);
+        $response->assertSessionHasErrors('number_of_people');
+        $errors = session('errors');
+        $this->assertEquals('来店人数を入力してください', $errors->first('number_of_people'));
+    }
+}
